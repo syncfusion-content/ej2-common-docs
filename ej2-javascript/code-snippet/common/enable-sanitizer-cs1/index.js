cc9061ad664868cea29587e9393dbe5ba089f8a4
Saranya13 <saranya.murugasamy@syncfusion.com>
2024-02-21 19:43:20 +0530

    867190: Added the changes

--- ej2-javascript/code-snippet/common/enable-sanitizer-cs1/index.js
+++ ej2-javascript/code-snippet/common/enable-sanitizer-cs1/index.js
@@ -3,3 +3,8 @@ var tooltip = new ej.popups.Tooltip({
     content: "<img src=text onerror=alert(`XSS_Script_Attack`) \/>" 
 });
 tooltip.appendTo('#target');
+
+var button = new ej.buttons.Button({
+    content: 'Hover me for a tooltip!',
+});
+button.appendTo('#target');
